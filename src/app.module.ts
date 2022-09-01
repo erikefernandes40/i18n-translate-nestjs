e56b60757378cd3a28b4d429c64aa11ab59fc609
erikefernandes40 <erikefernandes40@gmail.com>
2022-09-01 12:13:04 -0300

    feat: adiciona configuraçoes do i18n ao modulo raiz para ser global em toda aplicação

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { I18nModule } from 'nestjs-i18n';
+import path, { dirname } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      loaderOptions: {
+        path: path.join(__dirname, '/i18n/'),
+        watch: true,
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
