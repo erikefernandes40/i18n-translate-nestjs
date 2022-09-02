4ce047b400dfb026c0a1f4a2a511df2c8c0c6569
erikefernandes40 <erikefernandes40@gmail.com>
2022-09-02 08:56:33 -0300

    feat: adiciona funçao do i18n para puxar as traduçoes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { I18n, I18nContext } from 'nestjs-i18n';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +7,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@I18n() i18n: I18nContext) {
+    return this.appService.getHello(i18n);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { I18nContext } from 'nestjs-i18n';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getHello(i18n: I18nContext) {
+    return await i18n.t('card.CARD')
   }
 }
